1b3fbda5a82ed3f8ab08bbc9133330f350ecef96
Tranquility42 <neffetimahoud.dev@gmail.com>
2021-10-04 15:43:00 +0200

    add Entity Bloc

---
+++ src/Entity/Bloc.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BlocRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=BlocRepository::class)
+ */
+class Bloc
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=10000)
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $orderList;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getOrderList(): ?int
+    {
+        return $this->orderList;
+    }
+
+    public function setOrderList(int $orderList): self
+    {
+        $this->orderList = $orderList;
+
+        return $this;
+    }
+}
