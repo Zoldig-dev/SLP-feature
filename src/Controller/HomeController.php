6ed167609ef24baa8e0b7854199ebd5a48660d1c
Tranquility42 <neffetimahoud.dev@gmail.com>
2021-10-08 10:59:00 +0200

    add client twig & clients in home controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,20 +1,37 @@
 <?php
 
 namespace App\Controller;
-
+use App\Repository\HomePageClientRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    private HomePageClientRepository $clientRepository;
+
+    /**
+     * @param HomePageClientRepository $clientRepository
+     */
+    public function __construct(HomePageClientRepository $clientRepository)
+    {
+        $this->clientRepository = $clientRepository;
+    }
+
     /**
      * @Route("/home", name="home")
      */
     public function index(): Response
     {
+
+        $clients = $this->clientRepository->findAll();
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'clients' => $clients
         ]);
     }
+
+
 }
+
+
