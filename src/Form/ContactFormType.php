8f97c1e32514ee2d6bb671c8b4245da3288aac19
Zoldig <hraphaelh@gmail.com>
2021-10-12 16:27:41 +0200

    update contactForm scss

--- src/Form/ContactFormType.php
+++ src/Form/ContactFormType.php
@@ -4,6 +4,12 @@ namespace App\Form;
 
 use App\Entity\Contact;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,15 +18,48 @@ class ContactFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('isContact')
-            ->add('societyName')
-            ->add('firstName')
-            ->add('lastName')
-            ->add('email')
-            ->add('phone')
-            ->add('text')
-
-        ;
+            ->add('isContact', ChoiceType::class, [
+                'choices' => [
+                    'Contact' => true,
+                    'Demande de devis' => false
+                ],
+                "label" => "Type de demande",
+            ])
+            ->add('societyName', TextType::class, [
+                "attr" => [
+                    "placeholder" => "Société"
+                ]
+            ])
+            ->add('firstName', TextType::class, [
+                "attr" => [
+                    "placeholder" => "Nom"
+                ]
+            ])
+            ->add('lastName', TextType::class, [
+                "attr" => [
+                    "placeholder" => "Prenom"
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                "attr" => [
+                    "placeholder" => "E-Mail"
+                ]
+            ])
+            ->add('phone', TelType::class, [
+                "attr" => [
+                    "placeholder" => "Téléphone"
+                ]
+            ])
+            ->add('text', TextareaType::class, [
+//                "label" => "Votre message",
+                "attr" => [
+                    "rows" => "6",
+                    "placeholder" => "Votre message ..."
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Envoyer'
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
