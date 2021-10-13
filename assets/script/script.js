a0313c50b4d65cd7bda8b7d37220df5bd145dd30
Tranquility42 <neffetimahoud.dev@gmail.com>
2021-10-13 19:41:38 +0200

    add animation to burger menu

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,24 +2,35 @@ import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 // CKeditor ----------------------------------
 ClassicEditor
-    .create( document.querySelector( '#editor' ) )
-    .then( editor => {
-        console.log( editor );
-    } )
-    .catch( error => {
-        console.error( error );
-    } );
+    .create(document.querySelector('#editor'))
+    .then(editor => {
+        console.log(editor);
+    })
+    .catch(error => {
+        console.error(error);
+    });
 
 
-
-
-const navSlide = () =>{
+const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
 
-    burger.addEventListener('click',() => {
+
+    burger.addEventListener('click', () => {
+        //Toggle Nav
         nav.classList.toggle('nav-active');
-    });
+
+        //Animate Links
+        navLinks.forEach((link,index)=>{
+            if (link.style.animation){
+                link.style.animation ='';
+            }else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+            }
+        })
+     });
+
 }
 
 navSlide();
