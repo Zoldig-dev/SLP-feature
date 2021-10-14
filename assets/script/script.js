cee7459728cd4268fe36c14c2a78cec691113dfb
Zoldig <hraphaelh@gmail.com>
2021-10-14 18:40:58 +0200

    update correction mineur sur le sass général de la page home

--- assets/script/script.js
+++ assets/script/script.js
@@ -11,7 +11,7 @@ ClassicEditor
         console.error( error );
     } );
 
-
+// nav
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
