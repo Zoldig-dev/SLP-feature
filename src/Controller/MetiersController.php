04515c7cf2c934c4105a151ab01b96972e6e94e6
Zoldig <hraphaelh@gmail.com>
2021-10-21 11:30:17 +0200

    add pageCustom metiers + MetierController

---
+++ src/Controller/MetiersController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\BlocRepository;
+use App\Repository\PageCustomRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MetiersController extends AbstractController
+{
+    /**
+     * @var BlocRepository
+     */
+    private $blocRepo;
+    /**
+     * @var PageCustomRepository
+     */
+    private $pageRepo;
+
+    /**
+     * @param BlocRepository $blocRepo
+     * @param PageCustomRepository $pageRepo
+     */
+    public function __construct(BlocRepository $blocRepo, PageCustomRepository $pageRepo)
+    {
+        $this->blocRepo = $blocRepo;
+        $this->pageRepo = $pageRepo;
+    }
+
+    #[Route('/metiers', name: 'metiers')]
+    public function index(): Response
+    {
+        $pageCustom = $this->pageRepo->findAll();
+        $blocs = $this->blocRepo->findAll();
+
+        return $this->render('metiers/index.html.twig', [
+            'controller_name' => 'MetiersController',
+            'metiers' => $pageCustom,
+            'blocs' => $blocs,
+        ]);
+    }
+}
