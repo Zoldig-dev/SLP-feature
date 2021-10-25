11f29b267792ebf51dac1446fb122dadd49eaf34
Zoldig <hraphaelh@gmail.com>
2021-10-25 11:16:39 +0200

    update ancre #contact in nav

---
+++ migrations/Version20211025081216.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211025081216 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE bloc ADD name VARCHAR(255) NOT NULL, ADD is_image TINYINT(1) NOT NULL, DROP order_list, CHANGE content content LONGTEXT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE bloc ADD order_list INT NOT NULL, DROP name, DROP is_image, CHANGE content content LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
+    }
+}
